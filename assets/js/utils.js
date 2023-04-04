81a615fad218639638ab2cc8890a7e8fd0b1c3e9
Alexiis94 <alexiis.95.15@gmail.com>
2023-04-04 16:36:46 -0400

    style(app): adapted responsive views

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,18 +1,26 @@
-// window.addEventListener("scroll", () => {
-//   const btnScroll = document.getElementsByClassName("scroll-top");
-//   if (document.documentElement.scrollTop <= 450) {
-//     btnScroll.style.display = "none";
-//   } else {
-//     btnScroll.style.display = "block";
-//     btnScroll.style.display = "flex";
-//   }
-// });
-function handleScrollTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
+window.addEventListener("scroll", () => {
+  const btnScroll = document.getElementById("scroll-top");
+  if (document.documentElement.scrollTop <= 150) {
+    btnScroll.style.display = "none";
+  } else {
+    btnScroll.style.display = "block";
+    btnScroll.style.display = "flex";
+  }
+});
+
+const btnScrollTop = document.getElementById("scroll-top");
+if (btnScrollTop) {
+  btnScrollTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
 }
-function goBack() {
-  window.history.back();
+
+const btnGoBack = document.getElementById("btn-goBack");
+if (btnGoBack) {
+  btnGoBack.addEventListener("click", () => {
+    window.history.back();
+  });
 }
