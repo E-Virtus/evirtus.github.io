6715ed683d71d95cc1f2706fa77b2eec3ebfc4df
Alexiis94 <alexiis.95.15@gmail.com>
2023-05-25 17:50:54 -0400

    style(app): change color, add sliders in blog and posts views.

---
+++ _data/services.js
@@ -0,0 +1,28 @@
+const services = [
+  {
+    id: "607de3e212a615436826aef7",
+    name: "Breach and Attack Simulation",
+    url: "https://e-virtus.s3.us-east-2.amazonaws.com/CYMULATE_860x466_2411eb762b.jpg",
+  },
+  {
+    id: "60aed59358f1f058665f45fa",
+    name: "Vulnerability and Patch Management",
+    url: "https://e-virtus.s3.us-east-2.amazonaws.com/pexels_mati_mango_6330644_1_ca689fd6ea.jpg",
+  },
+  {
+    id: "62bf567f938d803f2de8c76c",
+    name: "Red Team",
+    url: "https://e-virtus.s3.us-east-2.amazonaws.com/e_virtus_com_513b0f4ddd.png",
+  },
+  {
+    id: "62bf5db1c8478e2b108e2ac1",
+    name: "DevSecOps",
+    url: "https://e-virtus.s3.us-east-2.amazonaws.com/E_Virtus_2022_Hacking_Continuo_v2_pptx_1_425d0a4499.png",
+  },
+  {
+    id: "62c352a9938d803f2de8c869",
+    name: "Managed Detection and Response",
+    url: "https://e-virtus.s3.us-east-2.amazonaws.com/MDR_da14144d23.png",
+  },
+];
+export { services };
